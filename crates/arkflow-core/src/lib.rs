3b552d7573611873c3da65187e73c644d46a3729
chenquan <chenquan.dev@gmail.com>
2025-03-21 21:59:40 +0800

    refactor: refactoring message

--- crates/arkflow-core/src/lib.rs
+++ crates/arkflow-core/src/lib.rs
@@ -1,7 +1,11 @@
 //! Rust stream processing engine
 
+use datafusion::arrow::array::{ArrayRef, BinaryArray};
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
 use datafusion::arrow::record_batch::RecordBatch;
 use serde::Serialize;
+use std::ops::{Deref, DerefMut};
+use std::sync::Arc;
 use thiserror::Error;
 
 pub mod cli;
@@ -41,6 +45,9 @@ pub enum Error {
     #[error("Timeout error")]
     Timeout,
 
+    #[error("Arrow error: {0}")]
+    Arrow(String),
+
     #[error("Unknown error: {0}")]
     Unknown(String),
 
@@ -53,31 +60,37 @@ pub type Bytes = Vec<u8>;
 /// Represents a message in a stream processing engine.
 
 #[derive(Clone, Debug)]
-pub struct MessageBatch {
-    /// Message content
-    pub content: Content,
-}
+pub struct MessageBatch(RecordBatch);
 
 #[derive(Clone, Debug)]
-pub enum Content {
-    Arrow(RecordBatch),
-    Binary(Vec<Bytes>),
+pub enum MessageType {
+    Binary,
+    Arrow,
 }
 
 impl MessageBatch {
     pub fn new_binary(content: Vec<Bytes>) -> Self {
-        Self {
-            content: Content::Binary(content),
+        let fields = vec![Field::new("value", DataType::Binary, false)];
+        let mut columns: Vec<ArrayRef> = Vec::with_capacity(content.len());
+
+        for x in content {
+            let array = BinaryArray::from_vec(vec![&x]);
+            columns.push(Arc::new(array))
         }
+
+        let schema = Arc::new(Schema::new(fields));
+        let batch = RecordBatch::try_new(schema, columns)
+            .map_err(|e| Error::Process(format!("创建Arrow记录批次失败: {}", e)))
+            .unwrap();
+
+        Self(batch)
     }
     pub fn from_json<T: Serialize>(value: &T) -> Result<Self, Error> {
         let content = serde_json::to_vec(value)?;
         Ok(Self::new_binary(vec![content]))
     }
     pub fn new_arrow(content: RecordBatch) -> Self {
-        Self {
-            content: Content::Arrow(content),
-        }
+        Self(content)
     }
 
     /// Create a message from a string.
@@ -85,37 +98,52 @@ impl MessageBatch {
         Self::new_binary(vec![content.as_bytes().to_vec()])
     }
 
-    /// Parse the message content into a string.
-    pub fn as_string(&self) -> Result<Vec<String>, Error> {
-        match &self.content {
-            Content::Arrow(_) => Err(Error::Process("无法解析为JSON".to_string())),
-            Content::Binary(v) => {
-                let x: Result<Vec<String>, Error> = v
-                    .iter()
-                    .map(|v| {
-                        String::from_utf8(v.clone())
-                            .map_err(|_| Error::Process("无法解析为字符串".to_string()))
-                    })
-                    .collect();
-                Ok(x?)
-            }
-        }
-    }
-    /// Get the binary content of the message.
-    pub fn as_binary(&self) -> &Vec<Bytes> {
-        match &self.content {
-            Content::Arrow(_) => panic!("Cannot get binary content from Arrow message"),
-            Content::Binary(v) => v,
-        }
-    }
-
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
     pub fn len(&self) -> usize {
-        match &self.content {
-            Content::Arrow(v) => v.num_rows(),
-            Content::Binary(v) => v.len(),
-        }
+        self.0.num_rows()
+    }
+}
+
+impl Deref for MessageBatch {
+    type Target = RecordBatch;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<RecordBatch> for MessageBatch {
+    fn from(batch: RecordBatch) -> Self {
+        Self(batch)
+    }
+}
+
+impl From<MessageBatch> for RecordBatch {
+    fn from(batch: MessageBatch) -> Self {
+        batch.0
+    }
+}
+
+impl From<Vec<Bytes>> for MessageBatch {
+    fn from(content: Vec<Bytes>) -> Self {
+        Self::new_binary(content)
+    }
+}
+impl From<Vec<String>> for MessageBatch {
+    fn from(content: Vec<String>) -> Self {
+        Self::new_binary(content.into_iter().map(|s| s.into_bytes()).collect())
+    }
+}
+impl From<Vec<&str>> for MessageBatch {
+    fn from(content: Vec<&str>) -> Self {
+        Self::new_binary(content.into_iter().map(|s| s.as_bytes().to_vec()).collect())
+    }
+}
+
+impl DerefMut for MessageBatch {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
