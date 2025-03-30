6cdcda3c3ddc643fa5be737de1c76c32c2b2cec7
chenquan <chenquan.dev@gmail.com>
2025-03-31 00:46:53 +0800

    doc: release v0.2.0

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -78,7 +78,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Doc',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
