035675dac60e7f0e30608e8646e7ef21eb9f2e63
chenquan <chenquan.dev@gmail.com>
2025-04-16 23:17:37 +0800

    perf(sql): Optimize SQL processor

--- crates/arkflow-plugin/src/processor/sql.rs
+++ crates/arkflow-plugin/src/processor/sql.rs
@@ -22,7 +22,10 @@ use async_trait::async_trait;
 use datafusion::arrow;
 use datafusion::arrow::datatypes::Schema;
 use datafusion::arrow::record_batch::RecordBatch;
+use datafusion::common::DataFusionError;
+use datafusion::optimizer::OptimizerConfig;
 use datafusion::prelude::*;
+use datafusion::sql::parser::Statement;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
@@ -40,12 +43,25 @@ pub struct SqlProcessorConfig {
 /// SQL processor component
 pub struct SqlProcessor {
     config: SqlProcessorConfig,
+    pub statement: Statement,
 }
 
 impl SqlProcessor {
     /// Create a new SQL processor component.
     pub fn new(config: SqlProcessorConfig) -> Result<Self, Error> {
-        Ok(Self { config })
+        let mut ctx = SessionContext::new();
+        datafusion_functions_json::register_all(&mut ctx)
+            .map_err(|e| Error::Process(format!("Registration JSON function failed: {}", e)))?;
+
+        let statement = ctx
+            .state()
+            .sql_to_statement(
+                &config.query,
+                ctx.state().options().sql_parser.dialect.as_str(),
+            )
+            .map_err(|e| Error::Process(format!("SQL query error: {}", e)))?;
+
+        Ok(Self { config, statement })
     }
 
     /// Execute SQL query
@@ -64,15 +80,10 @@ impl SqlProcessor {
             .map_err(|e| Error::Process(format!("Registration failed: {}", e)))?;
 
         // Execute the SQL query and collect the results.
-        let sql_options = SQLOptions::new()
-            .with_allow_ddl(false)
-            .with_allow_dml(false)
-            .with_allow_statements(false);
-        let df = ctx
-            .sql_with_options(&self.config.query, sql_options)
+        let df = self
+            .execute_query_with_statement(ctx)
             .await
-            .map_err(|e| Error::Process(format!("SQL query error: {}", e)))?;
-
+            .map_err(|e| Error::Process(format!("Execution query error: {}", e)))?;
         let result_batches = df
             .collect()
             .await
@@ -90,6 +101,24 @@ impl SqlProcessor {
             arrow::compute::concat_batches(&&result_batches[0].schema(), &result_batches)
                 .map_err(|e| Error::Process(format!("Batch merge failed: {}", e)))?,
         )
+    }
+
+    async fn execute_query_with_statement(
+        &self,
+        ctx: SessionContext,
+    ) -> Result<DataFrame, DataFusionError> {
+        let sql_options = SQLOptions::new()
+            .with_allow_ddl(false)
+            .with_allow_dml(false)
+            .with_allow_statements(false);
+
+        let plan = ctx
+            .state()
+            .statement_to_plan(self.statement.clone())
+            .await?;
+        sql_options.verify_plan(&plan)?;
+
+        ctx.execute_logical_plan(plan).await
     }
 }
 
