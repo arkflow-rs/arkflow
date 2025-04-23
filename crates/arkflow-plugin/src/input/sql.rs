ab7fa3e85be3a17d1817020de79c37bee325b40f
chenquan <chenquan.dev@gmail.com>
2025-04-23 21:00:02 +0800

    x

--- crates/arkflow-plugin/src/input/sql.rs
+++ crates/arkflow-plugin/src/input/sql.rs
@@ -19,7 +19,7 @@ use std::collections::HashMap;
 use async_trait::async_trait;
 
 use crate::processor::udf;
-use ballista::prelude::{SessionConfigExt, SessionContextExt};
+use ballista::prelude::SessionContextExt;
 use datafusion::execution::options::ArrowReadOptions;
 use datafusion::physical_plan::SendableRecordBatchStream;
 use datafusion::prelude::*;
