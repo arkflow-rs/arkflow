e6119b2bdbbb47e795ec69916824aa85d64161cc
chenquan <chenquan.dev@gmail.com>
2025-05-29 21:06:54 +0800

    x

--- crates/arkflow-plugin/src/input/modbus.rs
+++ crates/arkflow-plugin/src/input/modbus.rs
@@ -11,45 +11,227 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-use arkflow_core::input::{Ack, Input};
-use arkflow_core::{Error, MessageBatch};
+
+use arkflow_core::input::{Ack, Input, InputBuilder, NoopAck};
+use arkflow_core::{input, Error, MessageBatch, Resource};
 use async_trait::async_trait;
+use datafusion::arrow::array::{BooleanArray, RecordBatch, UInt16Array};
+use datafusion::arrow::buffer::OffsetBuffer;
+use datafusion::arrow::datatypes::Schema;
+use duckdb::arrow::array::{ArrayRef, ListArray};
+use duckdb::arrow::datatypes::{DataType, Field};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
-use tokio_modbus::prelude::tcp;
+use std::time::Duration;
+use tokio::sync::Mutex;
+use tokio_modbus::prelude::{tcp, Reader};
+use tokio_modbus::{Address, Quantity};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct ModbusInputConfig {
     addr: String,
+    points: Vec<Point>,
+    read_interval: Duration,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+enum PointType {
+    Coils,
+    DiscreteInputs,
+    HoldingRegisters,
+    InputRegisters,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Point {
+    #[serde(rename = "type")]
+    point_type: PointType,
+    name: String,
+    address: Address,
+    quantity: Quantity,
+}
+
 struct ModbusInput {
     config: ModbusInputConfig,
+    name: Option<String>,
+    first_read: AtomicBool,
+    client: Arc<Mutex<Option<tokio_modbus::client::Context>>>,
 }
 
 impl ModbusInput {
-    fn new(config: ModbusInputConfig) -> Self {
-        Self { config }
+    fn new(config: ModbusInputConfig, name: Option<String>) -> Self {
+        Self {
+            config,
+            first_read: AtomicBool::new(false),
+            client: Arc::new(Mutex::new(None)),
+            name,
+        }
     }
 }
 
 #[async_trait]
 impl Input for ModbusInput {
     async fn connect(&self) -> Result<(), Error> {
+        let mut cli_lock = self.client.lock().await;
         let socket_addr = self
             .config
             .addr
             .parse()
-            .map_err(|_| Error::from("Failed to parse socket address"))?;
+            .map_err(|_| Error::Process("Failed to parse socket address".to_string()))?;
 
-        let mut ctx = tcp::connect(socket_addr).await?;
+        let ctx = tcp::connect(socket_addr).await?;
+        cli_lock.replace(ctx);
         Ok(())
     }
 
     async fn read(&self) -> Result<(MessageBatch, Arc<dyn Ack>), Error> {
-        todo!()
+        let mut ctx = self.client.lock().await;
+        let Some(ctx) = ctx.as_mut() else {
+            return Err(Error::Disconnection);
+        };
+
+        if !self.first_read.load(std::sync::atomic::Ordering::SeqCst) {
+            tokio::time::sleep(self.config.read_interval).await;
+        } else {
+            self.first_read
+                .store(true, std::sync::atomic::Ordering::SeqCst);
+        }
+
+        let mut fields = Vec::with_capacity(self.config.points.len());
+        let mut array: Vec<ArrayRef> = Vec::with_capacity(self.config.points.len());
+        for x in &self.config.points {
+            match x.point_type {
+                PointType::Coils => {
+                    let result = ctx
+                        .read_coils(x.address, x.quantity)
+                        .await
+                        .map_err(|e| Error::Process(format!("Failed to read coils:{}", e)))?
+                        .map_err(|e| Error::Process(format!("Failed to read coils code:{}", e)))?;
+
+                    let (field, list_array) = Self::new_bool_list_array(&x.name, result)?;
+                    fields.push(field);
+                    array.push(list_array);
+                }
+                PointType::DiscreteInputs => {
+                    let result = ctx
+                        .read_discrete_inputs(x.address, x.quantity)
+                        .await
+                        .map_err(|e| {
+                            Error::Process(format!("Failed to read discrete inputs:{}", e))
+                        })?
+                        .map_err(|e| {
+                            Error::Process(format!("Failed to read discrete inputs code:{}", e))
+                        })?;
+                    let (field, list_array) = Self::new_bool_list_array(&x.name, result)?;
+                    fields.push(field);
+                    array.push(list_array);
+                }
+                PointType::HoldingRegisters => {
+                    let result = ctx
+                        .read_holding_registers(x.address, x.quantity)
+                        .await
+                        .map_err(|e| {
+                            Error::Process(format!("Failed to read holding registers:{}", e))
+                        })?
+                        .map_err(|e| {
+                            Error::Process(format!("Failed to read holding registers code:{}", e))
+                        })?;
+
+                    let (field, list_array) = Self::new_u16_list_array(&x.name, result)?;
+                    fields.push(field);
+                    array.push(list_array);
+                }
+                PointType::InputRegisters => {
+                    let result = ctx
+                        .read_input_registers(x.address, x.quantity)
+                        .await
+                        .map_err(|e| {
+                            Error::Process(format!("Failed to read input registers:{}", e))
+                        })?
+                        .map_err(|e| {
+                            Error::Process(format!("Failed to read input registers code:{}", e))
+                        })?;
+
+                    let (field, list_array) = Self::new_u16_list_array(&x.name, result)?;
+                    fields.push(field);
+                    array.push(list_array);
+                }
+            }
+        }
+        let batch = RecordBatch::try_new(Arc::new(Schema::new(fields)), array)
+            .map_err(|e| Error::Process(format!("Failed to create record batch:{}", e)))?;
+        let mut msg: MessageBatch = batch.into();
+        msg.set_input_name(self.name.clone());
+        Ok((msg, Arc::new(NoopAck)))
     }
 
     async fn close(&self) -> Result<(), Error> {
-        todo!()
+        let mut cli_lock = self.client.lock().await;
+        let _ = cli_lock.take();
+        Ok(())
     }
+}
+
+impl ModbusInput {
+    fn new_offset_buffer(n: usize) -> OffsetBuffer<i32> {
+        OffsetBuffer::<i32>::from_lengths([n])
+    }
+
+    fn new_bool_list_array(name: &str, data: Vec<bool>) -> Result<(Field, ArrayRef), Error> {
+        let field = Field::new(
+            name,
+            DataType::List(Arc::new(Field::new_list_field(DataType::Boolean, false))),
+            false,
+        );
+        let list_array = ListArray::try_new(
+            Arc::new(Field::new_list_field(DataType::Boolean, false)),
+            Self::new_offset_buffer(data.len()),
+            Arc::new(BooleanArray::from(data)),
+            None,
+        )
+        .map_err(|e| Error::Process(format!("Failed to create list array:{}", e)))?;
+        Ok((field, Arc::new(list_array)))
+    }
+
+    fn new_u16_list_array(name: &str, data: Vec<u16>) -> Result<(Field, ArrayRef), Error> {
+        let field = Field::new(
+            name,
+            DataType::List(Arc::new(Field::new_list_field(DataType::UInt16, false))),
+            false,
+        );
+        let list_array = ListArray::try_new(
+            Arc::new(Field::new_list_field(DataType::UInt16, false)),
+            Self::new_offset_buffer(data.len()),
+            Arc::new(UInt16Array::from(data)),
+            None,
+        )
+        .map_err(|e| Error::Process(format!("Failed to create list array:{}", e)))?;
+        Ok((field, Arc::new(list_array)))
+    }
+}
+
+struct ModbusInputBuilder;
+
+impl InputBuilder for ModbusInputBuilder {
+    fn build(
+        &self,
+        name: Option<&String>,
+        config: &Option<Value>,
+        _resource: &Resource,
+    ) -> Result<Arc<dyn Input>, Error> {
+        let config = config
+            .as_ref()
+            .ok_or(Error::Process("Modbus input config is missing".to_string()))?;
+        let config: ModbusInputConfig = serde_json::from_value(config.clone())
+            .map_err(|e| Error::Process(format!("Failed to parse modbus input config:{}", e)))?;
+        Ok(Arc::new(ModbusInput::new(config, name.cloned())))
+    }
+}
+
+pub fn init() -> Result<(), Error> {
+    input::register_input_builder("modbus", Arc::new(ModbusInputBuilder))?;
+    Ok(())
 }
